2f83fc22e749e86d24fb5d5c8eb9d55b8179599d
Benjamin Dally <benjamin.dally@icloud.com>
2018-08-01 12:55:10 -0700

    styling additions:

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,12 +4,15 @@ $(function() {
   };
   var navbar = document.getElementById("navbar");
   var sticky = navbar.offsetTop;
+  var iframe = document.getElementById("iframe_wrapper");
 
   function stickyNav() {
     if (window.pageYOffset >= sticky) {
       navbar.classList.add("sticky_nav");
+      iframe.classList.add("iframe_dont_move");
     } else {
       navbar.classList.remove("sticky_nav");
+      iframe.classList.remove("iframe_dont_move");
     }
   }
 });
