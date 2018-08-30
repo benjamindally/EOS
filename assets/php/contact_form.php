b8d44451484f8e5dc1f97ae4950d6a258171607b
Benjamin Dally <benjamin.dally@icloud.com>
2018-08-30 09:43:43 -0700

    checking php form

---
+++ assets/php/contact_form.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $emailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "rosalie@megankraft.com";
+
+    $headers = "From: " .$emailFrom;
+
+    $txt = "You have recieved an email from ".$name.".\n\n".$message;
+
+    mail($mailTo, $txt, $headers);
+
+    header("Location: index.html?mailsent");
+    
+}
+
